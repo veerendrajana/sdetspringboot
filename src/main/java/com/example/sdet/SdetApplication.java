613533c83e111f9d240982b4e5a0a14aeeb47d58
veerendra <veerendra.jana2010@gmail.com>
2024-10-29 20:29:26 +0530

    created springboot project

---
+++ src/main/java/com/example/sdet/SdetApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdetApplication.class, args);
+	}
+
+}
